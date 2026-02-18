fdcee75454725d39fcf27e69cde86813ec193f42
Saito-Dai <dai2525yuu@icloud.com>
2026-02-18 11:48:50 +0900

    SQL実装完了

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel, Field # Basemodelはデータの型定義。fieldは詳細なデータ制限を書けるために使う
 from sqlalchemy.orm import Session
 
-from db import SessionLocal, engine, Base
-from models import Todo
+from app.db import SessionLocal, engine, Base
+from app.models import Todo
 
 app = FastAPI(title="Day5 API Practice")
 
@@ -34,3 +34,32 @@ class TodoOut(BaseModel):
     title: str
     detail : str | None = None
     done : bool 
+
+@app.get("/")
+def read_root():
+    return {"message": "TodoAPIです"}
+
+@app.get("/health")
+def health_check():
+    return {"status" : "ok"}
+
+@app.get("/todos", response_model=List[TodoOut]) # response_modelはデータのフィルタリング、変換、ドキュメントへの反映を行う。
+def list_todos(db:Session = Depends(get_db)): # dbはSession型の入る変数であり、get_dbがyieldで一時的に渡したオブジェクトが入る。
+    rows = db.query(Todo).order_by(Todo.id).all() # todoテーブルデータを全て取得。idの小さい順に整列、.allでlist形式で渡す。
+    return [TodoOut(id=r.id, title=r.title, detail=r.detail, done=r.done) for r in rows] # DB専用の重いormモデル(Todo)を通信専用の軽いpydanticモデル(TodoOut)に詰め替え。
+
+@app.get("/todos/{todo_id}", response_model=TodoOut)
+def get_todo(todo_id :int, db:Session = Depends(get_db)):
+    row = db.query(Todo).filter(Todo.id == todo_id).first() # .filter(条件).first()は条件に適合する最初の一つを返す
+    if row is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return TodoOut(id=row.id, title=row.title, detail=row.detail, done=row.done)
+
+@app.post("/todos", response_model=TodoOut, status_code=status.HTTP_201_CREATED)
+def create_todo(payload: TodoCreate, db: Session = Depends(get_db)): # payloadはユーザーが送ってきたデータを示す。fastapiではjsonデータを指す。
+    row = Todo(title=payload.title, detail=payload.detail, done=False)
+    db.add(row) # git add 保存対象としてスタンバイ
+    db.commit() # git commit 変更を確定。
+    db.refresh(row)
+    
+    return TodoOut(id=row.id, title=row.title, detail=row.detail, done=row.done)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import String, Boolean, Text # DB側に作るデータ型の指定。strは短い。textは長い文字列で使うぞ。
 from sqlalchemy.orm import Mapped, mapped_column # DB列と宣言するためのペア。mappedは型ヒント、mapped_columnはDBにおける規則設定
-from db import Base # Baseクラスを使ってデータベースのテーブルを作るぞ
+from app.db import Base # Baseクラスを使ってデータベースのテーブルを作るぞ
 
 class Todo(Base):
     __tablename__ = "todos"
